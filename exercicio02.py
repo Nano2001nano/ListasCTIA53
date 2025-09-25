025ce9040bb4edd1278b620dd64d32ee65d00e98
Nano2001nano <netocaetano78@gmail.com>
2025-09-25 12:23:25 -0300

    exercicio02

---
+++ exercicio02.py
@@ -0,0 +1,8 @@
+horas = int(input("\nDigite a quantidade de horas para converter em segundos: "))
+#entrada de dados
+
+segundos = horas * 3600
+#processamento de dados
+
+print(f"\n{horas} horas equivalem a {segundos} segundos.\n")
+#saida de dados
