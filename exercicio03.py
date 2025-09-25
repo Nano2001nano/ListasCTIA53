d5e981a2f27a3ac15b10eaf9363080193bd5be81
Nano2001nano <netocaetano78@gmail.com>
2025-09-25 12:31:16 -0300

    exercicio03

---
+++ exercicio03.py
@@ -0,0 +1,8 @@
+salario = float(input("\nDigite o seu salário: "))
+#entrada de dados
+
+novo_salario = round(salario * 1.15,0)
+#processamento de dados
+
+print(f"\nSeu novo salário com 15% de aumento é: {novo_salario}\n")
+#saida de dados
