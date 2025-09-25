00befb397fed8e4349cc1e2386f2c8b01d385a30
Nano2001nano <netocaetano78@gmail.com>
2025-09-25 13:05:12 -0300

    exercicio07

---
+++ exercicio07.py
@@ -0,0 +1,12 @@
+nome = input("\nDigite o nome do estudante: ")
+nota1 = float(input("\nDigite a primeira nota: "))
+nota2 = float(input("\nDigite a segunda nota: "))
+nota3 = float(input("\nDigite a terceira nota: "))
+#entrada de dados
+
+soma = nota1 + nota2 + nota3
+media = soma / 3
+#processamento de dados
+
+print(f"\n O estudante {nome} obteve a média de {media}\n")
+#saída de dados
