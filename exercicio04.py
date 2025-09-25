881a0e2f1e3a8f47141accded44613103c0f090d
Nano2001nano <netocaetano78@gmail.com>
2025-09-25 12:43:34 -0300

    exercicio04

---
+++ exercicio04.py
@@ -0,0 +1,9 @@
+ano_nascimento = int(input("\nDigite seu ano de nascimento: "))
+#entrada de dados
+
+idade_atual = 2025 - ano_nascimento
+idade_2050 = 2050 - ano_nascimento
+#processamento de dados
+
+print(f"\nSua idade atual é {idade_atual} anos. E em 2050 você terá {idade_2050} anos.\n")
+#saída de dados
