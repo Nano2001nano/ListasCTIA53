c998118ab953b20270edd2b9c012831853762a58
Nano2001nano <netocaetano78@gmail.com>
2025-09-25 13:03:18 -0300

    exercicio06

---
+++ exercicio06.py
@@ -0,0 +1,8 @@
+temperatura = float(input("\nDigite a temperatura atual em graus Celsius: "))
+#entrada de dados
+
+fahrenheit = (temperatura * 9/5) + 32
+#processamento de dados
+
+print(f"\nA temperatura em Fahrenheit é: {fahrenheit}°F\n")
+#saída de dados
