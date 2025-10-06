745f8a2f250edbcca186fa14855176620d2e1483
Nano2001nano <netocaetano78@gmail.com>
2025-10-06 15:45:03 -0300

    desafio (Lista monitor Lucas)

---
+++ desafio.py
@@ -0,0 +1,14 @@
+valorA = float(input("Digite o valor A: "))
+valorB = float(input("Digite o valor B: "))
+valorC = float(input("Digite o valor C: "))
+#entrada de dados
+
+delta = (valorB ** 2) - (4 * valorA * valorC)
+x1 = (-valorB + delta ** 0.5) / (2 * valorA)
+x2 = (-valorB - delta ** 0.5) / (2 * valorA)
+#processamento de dados
+
+print(f"""O resultado do delta é: {delta}
+      O valor da raioz x1 é: {x1}
+      O valor da raiz x2 é: {x2}""")
+#saída de dados
